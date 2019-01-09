a356ef58a442d2a695115a6124041995bc49a4a9
Gabriel Hirakawa <gabriel.souza30@fatec.sp.gov.br>
2019-01-09 21:06:08 -0200

    Endpoint/{id} disponibilizado

--- src/main/java/com/gabriel/cursomc/resources/PedidoResource.java
+++ src/main/java/com/gabriel/cursomc/resources/PedidoResource.java
@@ -9,20 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gabriel.cursomc.domain.Categoria;
-import com.gabriel.cursomc.services.CategoriaService;
+import com.gabriel.cursomc.domain.Pedido;
+import com.gabriel.cursomc.services.PedidoService;
 
 @RestController
-@RequestMapping(value="/categorias")
-public class CategoriaResource {
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
 	
 	@Autowired
-	private CategoriaService service;
+	private PedidoService service;
 
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria obj = service.buscar(id);
+		Pedido obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	
 		
